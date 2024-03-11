d5c58070117717aa2c77d5df82e4f3e2780becfc
arkarsg <alxarkar@gmail.com>
2024-03-12 00:12:28 +0800

    Implement trigger function

--- voyage-backend/functions/createUser.js
+++ voyage-backend/functions/createUser.js
@@ -1,10 +1,21 @@
-/**
- * @param {string} name - Username of user
- * @return {string} name - Username of user
- */
-function createUser(name) {
-  return name;
-}
+/*
+Access the providers associated with the authEvent:
+const providers = authEvent.providers
+
+Functions run by Triggers are run as System users and have full access to Services, Functions, and MongoDB Data.
+
+Access a mongodb service:
+const collection = context.services.get("<SERVICE_NAME>").db("<DB_NAME>").collection("<COLL_NAME>");
+const doc = collection.findOne({ name: "mongodb" });
+
+Call other named functions if they are defined in your application:
+const result = context.functions.execute("function_name", arg1, arg2);
+
+Access the default http client and execute a GET request:
+const response = context.http.get({ url: <URL> })
+
+Learn more about http client here: https://www.mongodb.com/docs/atlas/app-services/functions/context/#context-http
+*/
 
 /**
  * Adds the user object to User collection in the database
@@ -12,18 +23,33 @@ function createUser(name) {
  * @param {Object} user - Object representing the user
  * @return {Object} user
  */
-function addUserToCollection(user) {
-  // TO DO
-  return user;
+async function addUserToCollection(authEvent) {
+  const usersCollection = context.services
+    .get('mongodb-atlas')
+    .db('dev_voyage_app')
+    .collection('User');
+
+  const user = createUser(authEvent.user);
+  usersCollection.insertOne(user);
+}
+
+/**
+ * @param {Object} userObj - User associated with the auth event
+ * @return {Object} User document
+ */
+function createUser(userObj) {
+  return {
+    _id: BSON.ObjectId(userObj.id),
+    email: data.email,
+  };
 }
 
 // Export for App Services use
-exports = createUser;
+exports = addUserToCollection;
 
 // Export for unit test use
 if (typeof module !== 'undefined') {
   module.exports = {
-    createUser,
     addUserToCollection,
   };
 }
