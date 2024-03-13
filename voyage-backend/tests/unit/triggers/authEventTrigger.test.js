4bd3157d4238b112409905872ba72d1f51a6372a
arkarsg <alxarkar@gmail.com>
2024-03-13 22:01:19 +0800

    Refactor test case

--- voyage-backend/tests/unit/triggers/authEventTrigger.test.js
+++ voyage-backend/tests/unit/triggers/authEventTrigger.test.js
@@ -1,24 +1,39 @@
+require('dotenv').config();
 // eslint-disable-next-line camelcase
 const { app_id } = require('../../../realm_config.json');
 const Realm = require('realm');
+const { MongoClient } = require('mongodb');
 
-const { sleep } = require('../../testUtils');
 const { voyageDb, usersCollection, validUser } = require('../../constants');
 
 let user;
-const app = new Realm.App(app_id); // eslint-disable-line camelcase
+let mongoClient;
+let app;
+let dbCollection;
+
+beforeAll(async () => {
+  jest.setTimeout(10000);
+  app = new Realm.App(app_id); // eslint-disable-line camelcase
+  const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_URI}/test?retryWrites=true&w=majority`;
+  mongoClient = new MongoClient(uri);
+  await mongoClient.connect();
+  dbCollection = mongoClient.db(voyageDb).collection(usersCollection);
+});
+
+afterAll(async () => {
+  await mongoClient.close();
+});
 
 /**
  * Clean up function
  * Removes the user from User collection and delete the user from the app
  */
 afterEach(async () => {
-  const db = user.mongoClient('mongodb-atlas').db(voyageDb);
-  await db.collection(usersCollection).deleteMany({});
+  await dbCollection.deleteMany({});
   await app.deleteUser(user);
 });
 
-test('Auth trigger creates a user in User collection', async () => {
+test('emailPassword trigger creates a user in User collection', async () => {
   await app.emailPasswordAuth.registerUser({
     email: validUser.email,
     password: validUser.password,
@@ -31,15 +46,7 @@ test('Auth trigger creates a user in User collection', async () => {
 
   user = await app.logIn(credentials);
 
-  // Give time for the Trigger to execute on Atlas
-  await sleep(1000);
-
-  const allUsers = user
-    .mongoClient('mongodb-atlas')
-    .db(voyageDb)
-    .collection(usersCollection);
-
-  const insertedUser = await allUsers.findOne({
+  const insertedUser = await dbCollection.findOne({
     email: validUser.email,
   });
 
