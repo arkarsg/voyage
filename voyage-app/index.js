2f8362c438014e84c9898e9bf7264df564ac5517
arkarsg <alxarkar@gmail.com>
2024-03-27 00:29:47 +0800

    Use single entry point

--- voyage-app/index.js
+++ voyage-app/index.js
@@ -3,11 +3,8 @@ import 'expo-dev-client';
 import 'react-native-get-random-values';
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import { AppWrapperNonSync } from './app/AppWrapperNonSync';
-import { AppWrapperSync } from './app/AppWrapperSync';
-import { SYNC_CONFIG } from './sync.config';
 import { FirebaseApp } from './app/FirebaseApp';
 
-const App = () => (SYNC_CONFIG.enabled ? <FirebaseApp /> : <AppWrapperNonSync />);
+const App = () => <FirebaseApp />;
 
 registerRootComponent(App);
