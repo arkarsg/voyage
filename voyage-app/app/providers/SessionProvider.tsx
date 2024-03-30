b4f9d9fb0e75d93eda400215f968c3a6788abf0d
arkarsg <alxarkar@gmail.com>
2024-03-30 23:12:38 +0800

    Start from base auth

--- voyage-app/app/providers/SessionProvider.tsx
+++ voyage-app/app/providers/SessionProvider.tsx
@@ -1,8 +1,8 @@
 import React, { createContext, useContext } from 'react';
-import { useStorageState } from '@app/hooks/useStorageState';
+import { useStorageState } from '../hooks/useStorageState';
 import FirebaseAuth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import type { IAuthContext } from '@app/types';
+import type { IAuthContext } from '../types';
 
 const auth = FirebaseAuth();
 
