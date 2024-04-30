ed0f0f8746cab84a4db28cf2dff4ccc8e841249a
arkarsg <alxarkar@gmail.com>
2024-04-30 21:31:41 +0800

    feat(core) implement schema validation for trip name

--- voyage-app/app/types/tripSchema.ts
+++ voyage-app/app/types/tripSchema.ts
@@ -1,9 +1,18 @@
 import type { FormData } from './tripForm';
 import { z, type ZodType } from 'zod';
 
-export const TripSchema: ZodType<FormData> = z.object({
-  tripName: z.string().max(30, { message: 'Trip name is too long' }),
-  tripDestination: z.string(),
-  startDate: z.string(),
-  endDate: z.string(),
-});
+function containsIllegalCharacters(str: string): boolean {
+  return str.match(/[$<>;{}]/) !== null;
+}
+
+export const TripSchema: ZodType<FormData> = z
+  .object({
+    tripName: z.string().max(30, { message: 'Trip name is too long' }),
+    tripDestination: z.string(),
+    startDate: z.string(),
+    endDate: z.string(),
+  })
+  .refine((data) => !containsIllegalCharacters(data.tripName), {
+    message: 'Trip name cannot contain special characters',
+    path: ['tripName'],
+  });
