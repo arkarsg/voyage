16ed149159f8bdd63a69abdd413e643b9735f027
arkarsg <alxarkar@gmail.com>
2024-06-29 19:20:10 +0800

    complete invite functions

--- voyage-backend/functions/inviteTripMember.js
+++ voyage-backend/functions/inviteTripMember.js
@@ -1,17 +1,85 @@
-function inviteTripMember(tripId, tripMemberId) {
-  // if user is not a member of trip, then return error
-  // if tripMember does not exist, return error
-  // if tripMember is already in the trip, return error
-  // else, call system inviteTripMember function
-  //
-  // Why do we need to call system function?
-  // We want to use Realm's context to get user data that called the function
-  // If we run this function as system to directly invite trip members, we will
-  // not have info of who sent the invite.
-  return { tripId, tripMemberId };
+async function inviteTripMember(tripId, invitedMemberId) {
+  const tripsCollection = context.services
+    .get('mongodb-atlas')
+    .db('dev_voyage_app')
+    .collection('Trip');
+
+  const usersCollection = context.services
+    .get('mongodb-atlas')
+    .db('dev_voyage_app')
+    .collection('Users');
+
+  try {
+    // Check if the trip exists
+    const trip = await tripsCollection.findOne({ _id: tripId });
+    if (!trip) {
+      return {
+        error: {
+          message: 'Trip does not exist.',
+        },
+      };
+    }
+
+    const canInvite = trip && trip.tripMembers.includes(context.user.id);
+
+    if (!canInvite) {
+      return {
+        error: {
+          message:
+            'Cannont invite users to trip without belonging to the trip.',
+        },
+      };
+    }
+
+    // Check if the user to be invited exists
+    const user = await usersCollection.findOne({ _id: invitedMemberId });
+    if (!user) {
+      return {
+        error: {
+          message: 'User does not exist.',
+        },
+      };
+    }
+
+    // Check if the user is already a member of the trip
+    const isMember =
+      trip.tripMembers && trip.tripMembers.includes(invitedMemberId);
+    if (isMember) {
+      return {
+        error: {
+          message: 'User is already a member of the trip.',
+        },
+      };
+    }
+
+    const isAlreadyInvited = user.invites && user.invites.includes(tripId);
+    if (isAlreadyInvited) {
+      return {
+        error: {
+          message: 'User is already invited to the trip.',
+        },
+      };
+    }
+
+    // Call system function to invite the member
+    const result = await context.functions.execute(
+      'addTripToUserInvites',
+      tripId,
+      invitedMemberId,
+    );
+    return result;
+  } catch (err) {
+    console.log('Error occurred:', err.message);
+    return {
+      error: {
+        message: err.message || 'Something went wrong. Please try again.',
+      },
+    };
+  }
 }
 
 exports = inviteTripMember;
+
 // Export for unit test use
 if (typeof module !== 'undefined') {
   module.exports = {
